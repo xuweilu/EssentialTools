b178daf437162c426537b65f65f977aed342451e
zhibeiyou <xuweiluwx@gmail.com>
2016-04-01 00:11:22 +0800

    the first unittest

--- EssentialTools.Tests/UnitTest1.cs
+++ EssentialTools.Tests/UnitTest1.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
 
 namespace EssentialTools.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private IDiscountHelper getTestObject() {
+            return new MinimumDiscountHelper();
+        }
         [TestMethod]
-        public void TestMethod1()
+        public void Discount_Above_100()
         {
+            //arrange
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+
+            //action
+            var discountedTotal = target.ApplyDispcount(total);
+
+            //assert
+            Assert.AreEqual(total * 0.9m, discountedTotal);
         }
     }
 }
---
+++ EssentialTools/Models/MinimumDiscountHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class MinimumDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDispcount(decimal totalParam)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
