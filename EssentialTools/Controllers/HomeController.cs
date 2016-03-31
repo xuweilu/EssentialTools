0f78051676b44c00e98c18a2b287cd8f3614a70d
zhibeiyou <xuweiluwx@gmail.com>
2016-03-31 22:38:18 +0800

    NinjectProgram

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EssentialTools.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products = {
+            new Product { Name = "Kayak",Category = "Watersports",Price = 275M},
+            new Product { Name = "Lifejacket",Category = "Watersports",Price = 48.95M},
+            new Product { Name = "Soccer ball",Category = "Soccer",Price = 19.50m},
+            new Product { Name = "xuweilu",Category = "Watersports",Price = 275M}
+        };
+        // GET: Home
+        public ActionResult Index()
+        {
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+            //IValueCalculator calc = new LinqValueCalculator();
+            //LinqValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
---
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
