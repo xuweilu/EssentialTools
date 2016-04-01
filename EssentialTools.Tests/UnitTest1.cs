ae69efcaf7b6a4fd5d0c0df2c175765279bc9be1
unknown <>
2016-04-01 11:12:34 +0800

    add three test method

--- EssentialTools.Tests/UnitTest1.cs
+++ EssentialTools.Tests/UnitTest1.cs
@@ -23,5 +23,40 @@ namespace EssentialTools.Tests
             //assert
             Assert.AreEqual(total * 0.9m, discountedTotal);
         }
+        [TestMethod]
+        public void Discount_Between_10_And_100() {
+            //arrange
+            IDiscountHelper target = getTestObject();
+
+            //action
+            decimal TenDollarDiscount = target.ApplyDispcount(10);
+            decimal HundredDollarDiscount = target.ApplyDispcount(100);
+            decimal FiftyDollarDiscount = target.ApplyDispcount(50);
+
+            //assert
+            Assert.AreEqual(5, TenDollarDiscount, "$10 discount is wrong");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 discount is wrong");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 discount is wrong");
+        }
+        [TestMethod]
+        public void Discount_Less_Than_10() {
+            //arrange
+            IDiscountHelper target = getTestObject();
+            //action
+            decimal discount5 = target.ApplyDispcount(5);
+            decimal discount0 = target.ApplyDispcount(0);
+            //assert
+            Assert.AreEqual(5, discount5);
+            Assert.AreEqual(0, discount0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Discount_Negative_Total() {
+            //arrange
+            IDiscountHelper target = getTestObject();
+            //action
+            target.ApplyDispcount(-1);
+
+        }
     }
 }
