2c56f33c52964d3990de780c92b4e0ef2deab2a4
zhibeiyou <xuweiluwx@gmail.com>
2016-04-01 14:36:07 +0800

    use mock to test

---
+++ EssentialTools.Tests/UnitTest2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using EssentialTools.Models;
+using System.Linq;
+
+namespace EssentialTools.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private Product[] products = {
+            new Product { Name = "Kayak",Category = "Watersports",Price = 275M},
+            new Product { Name = "Lifejacket",Category = "Watersports",Price = 48.95M},
+            new Product { Name = "Soccer ball",Category = "Soccer",Price = 19.50m},
+            new Product { Name = "Corner flag",Category = "Soccer",Price = 34.95M}
+        };
+        [TestMethod]
+        public void Sum_Products_Correctly()
+        {
+            //arrange
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDispcount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
+
+            //action
+            var result = target.ValueProducts(products);
+
+            //assert
+            Assert.AreEqual(products.Sum(e => e.Price), result);
+        }
+    }
+}
